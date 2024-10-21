7af8affc034f64dd039d24cba1b1cae629abfcc0
Cláudio Peruna <danielperuna2012@gmail.com>
2024-10-21 10:10:14 -0300

    declaração anti-plágio

--- vendaingressos/src/Comprovante.java
+++ vendaingressos/src/Comprovante.java
@@ -1,3 +1,14 @@
+/*******************************
+ Autor: Cláudio Daniel Figueredo Peruna
+ Componente Curricular: EXA863 - MI - PROGRAMAÇÃO
+ Concluído em: 19/10/2024
+ Declaro que este código foi elaborado por mim de forma individual e não contêm nenhum
+ trecho de código de outro colega ou de outro autor, tais como provindos de livros e
+ apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
+ de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
+ do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
+ ********************************/
+
 import java.util.Date;
 
 public class Comprovante {
